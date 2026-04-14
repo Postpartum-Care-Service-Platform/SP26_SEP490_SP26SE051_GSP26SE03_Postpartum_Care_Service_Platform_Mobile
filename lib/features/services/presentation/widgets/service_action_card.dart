c7795ba1f23faaa818523aef3b25e9d3de62349e
Skive1 <lugiabao1410@gmail.com>
2026-04-14 10:41:37 +0700

    feat: implement ServiceActionCard widget and integrate it into ServiceDashboard UI

--- lib/features/services/presentation/widgets/service_action_card.dart
+++ lib/features/services/presentation/widgets/service_action_card.dart
@@ -8,6 +8,7 @@ class ServiceActionCard extends StatefulWidget {
   final Widget? iconWidget;
   final String title;
   final VoidCallback onTap;
+  final bool isEnabled;
 
   const ServiceActionCard({
     super.key,
@@ -15,6 +16,7 @@ class ServiceActionCard extends StatefulWidget {
     this.iconWidget,
     required this.title,
     required this.onTap,
+    this.isEnabled = true,
   }) : assert(icon != null || iconWidget != null,
             'Either icon or iconWidget must be provided');
 
@@ -51,77 +53,139 @@ class _ServiceActionCardState extends State<ServiceActionCard>
     final scale = AppResponsive.scaleFactor(context);
 
     return GestureDetector(
-      onTapDown: (_) {
-        setState(() => _isPressed = true);
-        _controller.forward();
-      },
-      onTapUp: (_) {
-        setState(() => _isPressed = false);
-        _controller.reverse();
-        widget.onTap();
-      },
-      onTapCancel: () {
-        setState(() => _isPressed = false);
-        _controller.reverse();
-      },
+      onTapDown: widget.isEnabled
+          ? (_) {
+              setState(() => _isPressed = true);
+              _controller.forward();
+            }
+          : null,
+      onTapUp: widget.isEnabled
+          ? (_) {
+              setState(() => _isPressed = false);
+              _controller.reverse();
+              widget.onTap();
+            }
+          : null,
+      onTapCancel: widget.isEnabled
+          ? () {
+              setState(() => _isPressed = false);
+              _controller.reverse();
+            }
+          : null,
       child: ScaleTransition(
         scale: _scaleAnimation,
-        child: Container(
-        decoration: BoxDecoration(
-          color: AppColors.white,
-            borderRadius: BorderRadius.circular(20 * scale),
-            border: Border.all(
-              color: _isPressed
-                  ? AppColors.primary.withValues(alpha: 0.3)
-                  : AppColors.borderLight,
-              width: 1.2 * scale,
-            ),
-          boxShadow: [
-            BoxShadow(
-                color: _isPressed
-                    ? AppColors.primary.withValues(alpha: 0.1)
-                    : Colors.black.withValues(alpha: 0.05),
-                blurRadius: _isPressed ? 16 * scale : 12 * scale,
-                offset: Offset(0, _isPressed ? 6 * scale : 4 * scale),
-                spreadRadius: _isPressed ? 2 * scale : 0,
-            ),
-          ],
-        ),
-          padding: EdgeInsets.all(20 * scale),
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-              // Icon Section
-              widget.iconWidget != null
-                  ? SizedBox(
-                      width: 44 * scale,
-                      height: 44 * scale,
-                      child: widget.iconWidget,
-                    )
-                  : Icon(
-                      widget.icon,
-                      size: 44 * scale,
-                      color: AppColors.primary,
-                    ),
-              SizedBox(height: 16 * scale),
-              
-              // Title - Centered
-            Text(
-                widget.title,
-              style: AppTextStyles.arimo(
-                fontSize: 14 * scale,
-                fontWeight: FontWeight.w700,
-                color: AppColors.textPrimary,
+        child: Opacity(
+          opacity: widget.isEnabled ? 1.0 : 0.6,
+          child: Container(
+            decoration: BoxDecoration(
+              color: AppColors.white,
+              borderRadius: BorderRadius.circular(20 * scale),
+              border: Border.all(
+                color: _isPressed && widget.isEnabled
+                    ? AppColors.primary.withValues(alpha: 0.3)
+                    : AppColors.borderLight,
+                width: 1.2 * scale,
               ),
-                textAlign: TextAlign.center,
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
+              boxShadow: [
+                BoxShadow(
+                  color: _isPressed && widget.isEnabled
+                      ? AppColors.primary.withValues(alpha: 0.1)
+                      : Colors.black.withValues(alpha: 0.05),
+                  blurRadius: _isPressed && widget.isEnabled ? 16 * scale : 12 * scale,
+                  offset: Offset(0, _isPressed && widget.isEnabled ? 6 * scale : 4 * scale),
+                  spreadRadius: _isPressed && widget.isEnabled ? 2 * scale : 0,
+                ),
+              ],
             ),
-            ],
+            padding: EdgeInsets.all(20 * scale),
+            child: Stack(
+              clipBehavior: Clip.none,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    // Icon Section
+                    widget.iconWidget != null
+                        ? SizedBox(
+                            width: 44 * scale,
+                            height: 44 * scale,
+                            child: widget.isEnabled
+                                ? widget.iconWidget
+                                : GreyscaleWidget(child: widget.iconWidget!),
+                          )
+                        : Icon(
+                            widget.icon,
+                            size: 44 * scale,
+                            color: widget.isEnabled
+                                ? AppColors.primary
+                                : AppColors.textSecondary.withValues(alpha: 0.5),
+                          ),
+                    SizedBox(height: 16 * scale),
+
+                    // Title - Centered
+                    Text(
+                      widget.title,
+                      style: AppTextStyles.arimo(
+                        fontSize: 14 * scale,
+                        fontWeight: FontWeight.w700,
+                        color: widget.isEnabled
+                            ? AppColors.textPrimary
+                            : AppColors.textSecondary,
+                      ),
+                      textAlign: TextAlign.center,
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ],
+                ),
+                if (!widget.isEnabled)
+                  Positioned(
+                    top: -10 * scale,
+                    right: -10 * scale,
+                    child: Container(
+                      padding: EdgeInsets.all(4 * scale),
+                      decoration: const BoxDecoration(
+                        color: AppColors.white,
+                        shape: BoxShape.circle,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black12,
+                            blurRadius: 4,
+                            spreadRadius: 1,
+                          ),
+                        ],
+                      ),
+                      child: Icon(
+                        Icons.lock_rounded,
+                        size: 14 * scale,
+                        color: AppColors.textSecondary,
+                      ),
+                    ),
+                  ),
+              ],
             ),
+          ),
         ),
       ),
+    );
+  }
+}
+
+class GreyscaleWidget extends StatelessWidget {
+  final Widget child;
+  const GreyscaleWidget({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return ColorFiltered(
+      colorFilter: const ColorFilter.matrix(<double>[
+        0.2126, 0.7152, 0.0722, 0, 0,
+        0.2126, 0.7152, 0.0722, 0, 0,
+        0.2126, 0.7152, 0.0722, 0, 0,
+        0,      0,      0,      1, 0,
+      ]),
+      child: child,
     );
   }
 }
